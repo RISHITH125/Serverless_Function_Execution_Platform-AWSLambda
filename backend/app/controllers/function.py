9bf76a45a91ed2653d46e1cd07acac189933da11
Pushkar S <spushkar0071234@gmail.com>
2025-04-13 03:02:12 +0530

    Added CRUD operations for functions

--- backend/app/controllers/function.py
+++ backend/app/controllers/function.py
@@ -1,7 +1,6 @@
-from app.core import jwt
-from fastapi import APIRouter, Header, HTTPException, status
+from app.core.security import validate_user
+from fastapi import APIRouter, Header, HTTPException
 from app.models.function import Function
-from app.core.jwt import decode_jwt_token
 from app.database.mongodb import db
 
 router = APIRouter()
@@ -9,34 +8,69 @@ router = APIRouter()
 
 @router.post("/function")
 async def store_function(function: Function, authorization: str = Header(...)):
-    split_token = authorization.split(" ")
-    if len(split_token) != 2:  # Bearer token
-        raise HTTPException(400, "Invalid token format")
-    try:
-        username = decode_jwt_token(split_token[1])["sub"]
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired"
-        )
-    except jwt.InvalidTokenError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
-        )
-
-    if not await db["users"].find_one({"username": username}):
-        raise HTTPException(404, "User not found")
-
-    existing = await db["functions"].find_one(
-        {
-            "username": username,
-            "$or": [{"name": function.name}, {"route": function.route}],
-        }
-    )
-    if existing:
-        raise HTTPException(409, "Function with same name or route exists")
+    username = await validate_user(authorization)
 
     function_dict = function.dict()
     function_dict["username"] = username
     function_dict["language"] = function.language.value
-    await db["functions"].insert_one(function_dict)
+    try:
+        await db["functions"].insert_one(function_dict)
+    except Exception as e:
+        if "duplicate key error" in str(e):
+            raise HTTPException(409, f"Function with same name or route exists: {e}")
+        raise HTTPException(500, f"Error storing function: {e}")
     return {"message": "Function stored successfully"}
+
+
+@router.get("/function/{function_name}")
+async def get_function(function_name: str, authorization: str = Header(...)):
+    username = await validate_user(authorization)
+
+    function = await db["functions"].find_one(
+        {"username": username, "name": function_name}
+    )
+    if not function:
+        raise HTTPException(404, "Function not found")
+    function.pop("_id", None)
+    function.pop("username", None)
+    return function
+
+
+@router.delete("/function/{function_name}")
+async def delete_function(function_name: str, authorization: str = Header(...)):
+    username = await validate_user(authorization)
+
+    await db["functions"].delete_one({"username": username, "name": function_name})
+    return {"message": "Function deleted successfully"}
+
+
+@router.put("/function/{function_name}")
+async def update_function(
+    function_name: str, function: Function, authorization: str = Header(...)
+):
+    username = await validate_user(authorization)
+
+    function_to_update = await db["functions"].find_one(
+        {
+            "username": username,
+            "name": function_name,
+        }
+    )
+
+    if not function_to_update:
+        raise HTTPException(404, "Function not found")
+
+    await db["functions"].update_one(
+        {"_id": function_to_update["_id"]},
+        {
+            "$set": {
+                "name": function.name,
+                "route": function.route,
+                "language": function.language.value,
+                "code": function.code,
+                "timeout": function.timeout,
+            }
+        },
+    )
+
+    return {"message": "Function updated successfully"}
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,9 @@
+from fastapi import HTTPException
 import bcrypt
+
+from app.core.jwt import decode_jwt_token
+from app.core import jwt
+from app.database.mongodb import db
 
 
 def hash_password(password: str) -> str:
@@ -7,3 +12,19 @@ def hash_password(password: str) -> str:
 
 def check_passwords_match(plain: str, hashed: str) -> bool:
     return bcrypt.checkpw(plain.encode(), hashed.encode())
+
+
+async def validate_user(authorization: str):
+    split_token = authorization.split(" ")
+    if len(split_token) != 2:
+        raise HTTPException(400, "Invalid token format")
+    try:
+        username = decode_jwt_token(split_token[1])["sub"]
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    if not await db["users"].find_one({"username": username}):
+        raise HTTPException(404, "User not found")
+    return username
